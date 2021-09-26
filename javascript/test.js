b407df0803eb3357e0000f53b3c4c45078633b74
deepanshu <deepanshukhore851@gmail.com>
2021-09-26 20:34:31 +0530

    first commit

---
+++ javascript/test.js
@@ -0,0 +1,14 @@
+<html>
+
+    <head>
+        <title>Testing js</title>
+        <script src=""></script>
+        <script type="text/javascipt">
+            document.write("Hello world")
+
+        </script>
+
+
+
+    </head>
+</html>
